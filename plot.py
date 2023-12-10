8a9781d05d82d26847e99ac3642cc59af43aa72e
Alexander2116 <Aleksander2116k@gmail.com>
2023-12-10 12:26:29 +0000

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,19 +2,22 @@ import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
 
-for i in range(100):
-    path = ".\\data2\\clean_Al_" + str(i) + ".csv"
-    data = pd.read_csv(path)
-    plt.plot(data,color="black")
+def do_av(path:str, len:int, points:int):
+    stack = np.zeros((1,points))
+    for i in range(len):
+        _path = path + str(i) + ".csv"
+        data = np.genfromtxt(_path,delimiter=',')
+        stack = np.vstack((stack, data))
+    stack = stack[1::]
+    return np.mean(stack,axis=0)
 
-"""for i in range(100):
-    path = ".\\data2\\laser_blue_Al_" + str(i) + ".csv"
-    data = pd.read_csv(path)
-    plt.plot(data,color="blue")
-"""
-for i in range(100):
-    path = ".\\data2\\laser_Al_" + str(i) + ".csv"
-    data = pd.read_csv(path)
-    plt.plot(data,color="green")
+
+LEN = 300
+POINTS = 247
+
+plt.plot(do_av(".\\data2\\clean_Al_",LEN,POINTS),color="black")
+plt.plot(do_av(".\\data2\\laser_blue_Al_",LEN,POINTS),color="blue")
+plt.plot(do_av(".\\data2\\laser_Al_",LEN,POINTS),color="green")
 
 plt.show()
+

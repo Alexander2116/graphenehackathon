330cdac4586c31b45fd23cf2adc17d5c08abe6a8
Alexander2116 <Aleksander2116k@gmail.com>
2023-12-10 11:58:16 +0000

    data

--- oscilloscope.py
+++ oscilloscope.py
@@ -16,13 +16,38 @@ def show_resources():
     
 show_resources()
 
-inst = pymeasure.instruments.keysight.KeysightDSOX1102G('USB0::0x2A8D::0x1797::CN57246135::INSTR')
 
+class Sensor():
+    def __init__(self, visa_str:str = "USB0::0x2A8D::0x1797::CN57246135::INSTR"):
+        self.instr = pymeasure.instruments.keysight.KeysightDSOX1102G('USB0::0x2A8D::0x1797::CN57246135::INSTR')
+        self.instr.acquisition_mode = "realtime"
+    
+    def take_data(self, ch: str = "channel1", data_points: int = 250) -> np.array:
+        self.instr.run()
+        data: np.array = self.instr.download_data(ch, data_points)[0]
+        self.instr.stop()
+        return data
+    
+    def save_data(path:str, data:np.array):
+        np.savetxt(path,data)
+
+    def predict_laser(self) -> (int,int,int,int):
+        # ML model that predicts if the laser is on or off
+        # return [on/off, r, g, b]
+        data = self.take_data()
+
+
+#inst = Sensor()
+
+
+
+inst = pymeasure.instruments.keysight.KeysightDSOX1102G('USB0::0x2A8D::0x1797::CN57246135::INSTR')
 inst.run()
 inst.acquisition_mode = "realtime"
-csv_path = ".\\data\\"
-for i in range(50):
-    csv_name ="clean" + str(i) + ".csv"
+csv_path = ".\\data2\\"
+for i in range(300):
+    csv_name ="laser_blue_Al_" + str(i) + ".csv"
     data: np.array = inst.download_data("channel1",250)[0]
     np.savetxt(csv_path+csv_name,data)
 inst.stop()
+
--- plot.py
+++ plot.py
@@ -3,12 +3,17 @@ import numpy as np
 import pandas as pd
 
 for i in range(100):
-    path = ".\\data\\clean_Al_" + str(i) + ".csv"
+    path = ".\\data2\\clean_Al_" + str(i) + ".csv"
+    data = pd.read_csv(path)
+    plt.plot(data,color="black")
+
+"""for i in range(100):
+    path = ".\\data2\\laser_blue_Al_" + str(i) + ".csv"
     data = pd.read_csv(path)
     plt.plot(data,color="blue")
-
+"""
 for i in range(100):
-    path = ".\\data\\laser_Al_" + str(i) + ".csv"
+    path = ".\\data2\\laser_Al_" + str(i) + ".csv"
     data = pd.read_csv(path)
     plt.plot(data,color="green")
 
